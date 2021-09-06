7c32f975547ca30d4ded6fdb82dce3dd7183e797
Jinbi Jin <jinbijin@jinbijin.nl>
2021-09-06 20:32:26 +0000

    Add short-circuiting test

--- src/maybe/maybe.spec.ts
+++ src/maybe/maybe.spec.ts
@@ -34,25 +34,44 @@ describe('Maybe', () => {
     });
 
     describe('get', () => {
-        it('should return value for just', () => {
-            const actual = Maybe.just(1).get();
+        describe('just', () => {
+            it('should return value for just', () => {
+                const actual = Maybe.just(1).get();
 
-            expect(actual).toEqual(1);
+                expect(actual).toEqual(1);
+            });
         });
 
-        it('should return null for none', () => {
-            const actual = Maybe.none.get();
+        describe('none', () => {
+            it('should return null for none', () => {
+                const actual = Maybe.none.get();
 
-            expect(actual).toEqual(null);
+                expect(actual).toEqual(null);
+            });
         });
 
-        it('should perform function', () => {
-            const actual = Maybe.from(function* () {
-                const justOne = Maybe.just(1);
-                return (yield* from(justOne)) + (yield* from(justOne));
-            }).get();
+        describe('from', () => {
+            it('should execute generator', () => {
+                const actual = Maybe.from(function* () {
+                    const justOne = Maybe.just(1);
+                    return (yield* from(justOne)) + (yield* from(justOne));
+                }).get();
 
-            expect(actual).toEqual(2);
+                expect(actual).toEqual(2);
+            });
+
+            it('should short-circuit on null', () => {
+                const stub = jest.fn();
+
+                const actual = Maybe.from(function* () {
+                    const value = yield* from(Maybe.none);
+                    stub();
+                    return value;
+                }).get();
+
+                expect(actual).toEqual(null);
+                expect(stub).not.toHaveBeenCalled();
+            });
         });
     });
 
